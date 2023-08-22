66b4d4f91504dedffb92a9e95f5f4fa13aab38c1
TranManhThang <tranmanhthang96@gmail.com>
2023-08-23 00:14:15 +0700

    learn tailwind css and reponsive

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "pink-test": "#AD26FF",
+      },
+    },
+  },
+  plugins: [],
+}
+
